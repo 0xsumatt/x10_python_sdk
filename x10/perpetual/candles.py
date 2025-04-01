e9e3ebc6ee2c5c7a967dc82ff51359b43b6427a4
sumatt <notaquant@protonmail.com>
2025-04-01 16:09:53 +0100

    fixed some imports

--- x10/perpetual/candles.py
+++ x10/perpetual/candles.py
@@ -6,18 +6,6 @@ from pydantic import AliasChoices, Field
 from x10.utils.model import X10BaseModel
 
 CandleType = Literal["trades", "mark-prices", "index-prices"]
-<<<<<<< HEAD
-CandleInterval = Literal["PT1M", "PT5M", "PT15M", "PT30M", "PT1H", "PT2H", "PT4H", "P1D"]
-
-
-class CandleModel(X10BaseModel):
-    open: Decimal = Field(validation_alias=AliasChoices("open", "o"), serialization_alias="o")
-    low: Decimal = Field(validation_alias=AliasChoices("low", "l"), serialization_alias="l")
-    high: Decimal = Field(validation_alias=AliasChoices("high", "h"), serialization_alias="h")
-    close: Decimal = Field(validation_alias=AliasChoices("close", "c"), serialization_alias="c")
-    volume: Decimal = Field(validation_alias=AliasChoices("volume", "v"), serialization_alias="v")
-    timestamp: int = Field(validation_alias=AliasChoices("timestamp", "T"), serialization_alias="T")
-=======
 CandleInterval = Literal[
     "PT1M", "PT5M", "PT15M", "PT30M", "PT1H", "PT2H", "PT4H", "P1D"
 ]
@@ -42,4 +30,3 @@ class CandleModel(X10BaseModel):
     timestamp: int = Field(
         validation_alias=AliasChoices("timestamp", "T"), serialization_alias="T"
     )
->>>>>>> change-to-ruff
