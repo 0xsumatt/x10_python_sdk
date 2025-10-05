d371a3f54ea8fe41cb63687b27118dc5675ce3d8
sumatt <notaquant@protonmail.com>
2025-10-05 19:46:41 +0100

    f

--- x10/config.py
+++ x10/config.py
@@ -1,4 +1,4 @@
-import importlib.metadata
+
 
 TRADING_API_URL_DEV = "http://api.testnet.extended.exchange/api/v1"
 STREAM_API_URL_DEV = "wss://api.testnet.extended.exchange/stream.extended.exchange/v1"
@@ -9,5 +9,5 @@ ADA_USD_MARKET = "ADA-USD"
 ETH_USD_MARKET = "ETH-USD"
 
 DEFAULT_REQUEST_TIMEOUT_SECONDS = 300
-SDK_VERSION = importlib.metadata.version("x10-python-trading-starknet")
+SDK_VERSION = 0.04
 USER_AGENT = f"X10PythonTradingClient/{SDK_VERSION}"
